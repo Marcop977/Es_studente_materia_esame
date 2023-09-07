58b6ccf258c434273e267c942b2d4c43c693334c
Marcop977 <136688345+Marcop977@users.noreply.github.com>
2023-09-07 23:23:15 +0200

    Second commit

--- Es_studente_materia_esame/src/gestoreesami/GestoreEsami.java
+++ Es_studente_materia_esame/src/gestoreesami/GestoreEsami.java
@@ -40,31 +40,38 @@ public class GestoreEsami {
 				materie.add(materia);
 			}
 			reader2.close();
-			
+
 			Scanner s = new Scanner(System.in);
-			
+
 			boolean condizione = true;
 			while(condizione) {
-				System.out.println("Seleziona ID dello studente (da 1 a 5), 7 per scrivere gli studenti, 0 per uscire:");
+				System.out.println("Seleziona ID dello studente (da 1 a 5), 7 per scrivere gli studenti, 8 per calcolare la media dei voti di tutti gli studenti, 9 per calcolare la media di tutti gli studenti per ogni materia, 0 per uscire:");
 				int sceltaStudente = s.nextInt();
 				if(sceltaStudente > 0 && sceltaStudente < studenti.size()) {
 					System.out.println(studenti.get(sceltaStudente - 1));
 					System.out.println("Seleziona ID materia (da 1 a 5):");
 					int sceltaMateria = s.nextInt();
 					System.out.println(materie.get(sceltaMateria - 1));
-					System.out.println("Inserisci voto per ");
+					System.out.println("Inserisci voto per " + materie.get(sceltaMateria - 1).getNome());
 					int voto = s.nextInt();
 					Esame esame = new Esame(studenti.get(sceltaStudente - 1), materie.get(sceltaMateria - 1), voto);
 					esami.add(esame);
 					System.out.println(esame);
-					
+
 				}else if(sceltaStudente == 7) {
 					PrintWriter pw = new PrintWriter(out);
 
 					for (Esame esame : esami) {
 						pw.println(esame);
 					}
-					pw.close();					
+					pw.close();	
+				}else if(sceltaStudente == 8) {
+					System.out.println(calcolaMediaStudenti(esami));
+				}else if(sceltaStudente == 9) {
+					System.out.println("Inserisci l'ID della materia:");
+					int materiaScelta = s.nextInt();
+					System.out.println(materie.get(materiaScelta - 101));
+					System.out.println(calcolaMediaStudentiMateria(esami, materiaScelta));					
 				}else if(sceltaStudente == 0){
 					for (Esame esame : esami) {
 						System.out.println(esame);
@@ -79,7 +86,7 @@ public class GestoreEsami {
 				}
 			}
 
-			
+
 		} catch (IOException e) {
 			System.out.println("Errore nella lettura del file: " + e.getMessage());
 		}
@@ -89,16 +96,42 @@ public class GestoreEsami {
 
 
 	public static double calcolaMediaStudenti(ArrayList<Esame> esami) {
-		int totale = 0;
+		double totale = 0;
 		int numEsami = 0;
 
 		for (Esame esame : esami) {
 			totale += esame.getVoto();
 			numEsami++;
 		}
-		
+
 		return totale / numEsami;
 	}
 
+	public static double calcolaMediaStudentiMateria(ArrayList<Esame> esami, int codiceMateria) {
+		double totale = 0;
+		int numMaterie = 0;
+
+		for (Esame esame : esami) {
+			if(codiceMateria == (esame.getMateria().getCodice())) {
+				totale += esame.getVoto();  //tutti i voti della materia corrispondente all'input utente
+				numMaterie++;
+			}
+		}
+		
+		return totale / numMaterie;		
+	}
+	
+	public static double calcolaMediaMateria(ArrayList<Esame> esami, int codiceMateria) {
+		double voto = 0;
+		for (Esame esame : esami) {
+			if(codiceMateria == esame.getMateria().getCodice()) {
+				voto = esame.getVoto();
+			}
+			
+		}
+		return voto;
+	}
 }
+
+
 
